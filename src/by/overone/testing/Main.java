37de44b0f262b33e59f7e0d013ed0a99bdec7450
RGNRK80 <armhound@gmail.com>
2021-10-11 10:20:49 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/by/overone/lesson13/Lesson13_Main.java

--- src/by/overone/testing/Main.java
+++ src/by/overone/testing/Main.java
@@ -3,7 +3,7 @@ package by.overone.testing;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<Double> listdd=new ArrayList<Double>();
-    }
+    String x= "Java".concat("Rules");
+
+
 }
